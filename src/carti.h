0be68807ea0d24ed9e5d5a6e90baeee0d8441040
Ty3r0X <ty3r0x@disroot.org>
2023-03-15 19:51:04 +0200

    oopsie

---
+++ src/carti.h
@@ -0,0 +1,23 @@
+/* include guard pentru
+ * orice eventualitate */
+#ifndef CARTI_H
+#define CARTI_H
+
+/* Proprietatile unei carti
+ * de joc sunt declarate pe
+ * aceasta structura */
+
+typedef struct {
+   char val;
+   char culoare;
+   char simbol;
+}carte;
+
+/* Prototpurile functiilor */
+
+char transformare(char n, int nr);
+void ordonare_init(carte *carti);
+void amestecare(carte *carti);
+void afisare(carte *carti);
+
+#endif
